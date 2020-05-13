a43d8fbb62ebe1aa2e7987c62806190b058ca230
Israel Canessa <israellcanessa@gmail.com>
2020-05-13 22:48:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
 array = Array.new
-array << wow << I << am << really << learning 
+array << wow << I << am << really << learning
+element = "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
