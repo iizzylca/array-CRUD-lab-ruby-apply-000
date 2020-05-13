945dd9f126fa9d4953849f2d46f187f5d0c01dca
Israel Canessa <israellcanessa@gmail.com>
2020-05-13 23:50:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,7 +35,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  index_number = array[0]
+  "#{index_number}"
 end
 
 def retrieve_last_element_from_array(array)
