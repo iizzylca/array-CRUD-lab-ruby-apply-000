b9fbbb83a221e56b2398daf3117a56d3d080c383
Israel Canessa <israellcanessa@gmail.com>
2020-05-13 23:29:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,9 +10,6 @@ def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
   element = "arrays!"
   array.insert "#{element}"
-
-
-
 end
 
 def add_element_to_start_of_array(array, element)
